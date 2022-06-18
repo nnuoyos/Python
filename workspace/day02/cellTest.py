0fc855c2d2f4a35e6b2d31e9fa342a5007fd1eeb
so young <104718503+nnuoyos@users.noreply.github.com>
2022-06-19 00:24:16 +0900

    2nd commit

---
+++ workspace/day02/cellTest.py
@@ -0,0 +1,8 @@
+#%% (1) first cell
+print("홍길동")
+print("이순신")
+print("강감찬")
+#%%(2) second cell
+print("을지문덕")
+print("장보고")
+print("장발장")
---
+++ workspace/day02/printTest.py
@@ -0,0 +1,7 @@
+#이름() => 함수, 메서드
+#주석 Mac단축키: command + 1 , win단축키: ctrl+1 
+#범위 주석 : command + 4 , 범위주석 해제 : command + 5
+print("홍길동")
+print("10살") 
+
+
