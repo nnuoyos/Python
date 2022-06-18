2a8a4c88072278e85c7a1e0a4e6a4aa5319d7725
so young <104718503+nnuoyos@users.noreply.github.com>
2022-06-19 00:18:09 +0900

    0619 1st commit

---
+++ baekjoon/bj_18108.py
@@ -0,0 +1,18 @@
+# #include <stdio.h>
+# int main() {
+
+# int bulY;
+# int seoY;
+
+# bulY= (1000 <= y <=  3000);
+# seoY=bulY-543
+
+# scanf("%d %d", &bulY, &seoY);
+
+
+# }
+
+
+bulY = int(input())
+seoY = bulY-543
+print(seoY)
