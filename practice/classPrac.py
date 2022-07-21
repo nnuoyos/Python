706a62fa654245a0a66245d88c70f5a14da00a4f
so young <104718503+nnuoyos@users.noreply.github.com>
2022-07-21 17:49:36 +0900

    파이썬 과제 클래스와 객체

--- practice/classPrac.py
+++ practice/classPrac.py
@@ -23,22 +23,31 @@ Lee=Friend("이선준", '010-7410-0258')
 Jang=Friend("장지우", '010-9630-0258')
 Heo2=Friend("허진욱", '010-4321-1234')
 
-# print(Heo.get_name())
-# print(Heo.get_phone())
-# print(Heo.set_phone('010-1234-5678'))
-# print(Heo.show_info())
+print(Heo.get_name())
+print(Heo.get_phone())
+print(Heo.set_phone('010-1234-5678'))
+print(Heo.show_info())
 
 
 # 문제2) 클래스 Friend를 기반으로 객체를 생성해 리스트에 담고 for문으로 info를 출력하자
 
 contact_list =[Heo, Lee, Jang, Heo2]
 
-# for i in contact_list:
-#     print(i.show_info())
+for i in contact_list:
+    print(i.show_info())
 
 # 문제3) 리스트에 담긴 객체 중 성이 '허' 인 사람의 info를 전부 출력하는 for문 만들기
 # s.startswith(prefix) => 문자열 s가 prefix로 시작하면 True, 아니면 False
 
 for i in contact_list:
-    if (i.get_name().startswith('허'))==True:
+    if (i.get_name().startswith('장'))==True:
+        print(i.show_info())
+
+
+# 문제4) 리스트에 담긴 객체들 중 '장지우' 의 전화번호를 수정하는 코드를 for문으로 작성하기 
+# 장지우 010-8520-0147 수정 완료 되면 '장지우' 정보 찾아서 출력하는 for문 작성하기
+
+for i in contact_list:
+    if (i.get_name().startswith('장'))==True:
+        i.set_phone('010-8520-0147')
         print(i.show_info())
